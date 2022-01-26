aa9e3b4bea047c3213b0d2881516bab83a940f21
benccalcyxzfi <88748637+benccalcyxzfi@users.noreply.github.com>
2022-01-26 18:56:26 -0300

    create logical

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 /* Regras Codificador: 
 "e" é convertido para "enter" 
 "i" é convertido para "imes"
@@ -18,3 +17,57 @@ Não permite acentuação
 Apenas letras minúsculas
 Não permite acentuação     
 */
+var items = [
+  ["e", "enter"], 
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"]
+];
+
+function Codificar(texto) {
+
+  items.forEach((element) => {
+    var textoProcurar = element[0];
+    var textoSubstituir = element[1];
+    texto=texto.replaceAll(textoProcurar, textoSubstituir);
+    console.log(texto.replaceAll(textoProcurar, textoSubstituir));
+  });
+  return texto;
+}
+
+function Decodificar(texto) {
+
+    items.forEach((element) => {
+      var textoProcurar = element[1];
+      var textoSubstituir = element[0];
+      texto=texto.replaceAll(textoProcurar, textoSubstituir);
+    });
+
+    return texto;
+  }
+  function copiar(campo){
+        campo.select();
+        campo.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(campo.value);
+        alert("O texto foi copiado");
+  }
+
+  document.getElementById("btn-cripto").addEventListener("click", function () {
+    var textoDecodificado = document.getElementById("input-texto").value;
+    if(textoDecodificado.toLowerCase() != textoDecodificado){
+
+      alert("maiuscula!");
+    }
+    var textoCodificado = Codificar(textoDecodificado);
+    document.getElementById("msg").value = textoCodificado;
+  });
+  document.getElementById("btn-descripto").addEventListener("click", function () {
+      var textoCodificado = document.getElementById("input-texto").value;
+      var textoDecodificado = Decodificar(textoCodificado);
+      document.getElementById("msg").value = textoDecodificado;
+    });
+    document.getElementById("btn-copy").addEventListener("click", function () {
+      var inputMensagem = document.getElementById("msg");
+      copiar(inputMensagem);
+    });
