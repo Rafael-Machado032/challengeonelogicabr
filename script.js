2100ba2d54a34638aaa372946effa82ad04ec457
benccalcyxzfi <88748637+benccalcyxzfi@users.noreply.github.com>
2022-01-26 19:38:52 -0300

    create rulers to validate

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
-
 var items = [
-  ["e", "enter"], 
+  ["e", "enter"],
   ["i", "imes"],
   ["a", "ai"],
   ["o", "ober"],
-  ["u", "ufat"]
+  ["u", "ufat"],
 ];
 
 function Codificar(texto) {
-
   items.forEach((element) => {
     var textoProcurar = element[0];
     var textoSubstituir = element[1];
-    texto=texto.replaceAll(textoProcurar, textoSubstituir);
+    texto = texto.replaceAll(textoProcurar, textoSubstituir);
     console.log(texto.replaceAll(textoProcurar, textoSubstituir));
   });
   return texto;
 }
 
 function Decodificar(texto) {
-
-    items.forEach((element) => {
-      var textoProcurar = element[1];
-      var textoSubstituir = element[0];
-      texto=texto.replaceAll(textoProcurar, textoSubstituir);
-    });
-
-    return texto;
-  }
-  function copiar(campo){
-        campo.select();
-        campo.setSelectionRange(0, 99999);
-        navigator.clipboard.writeText(campo.value);
-        alert("O texto foi copiado");
-  }
-
-  document.getElementById("btn-cripto").addEventListener("click", function () {
-    var textoDecodificado = document.getElementById("input-texto").value;
-    if(textoDecodificado.toLowerCase() != textoDecodificado){
-
-      alert("maiuscula!");
-    }
-    var textoCodificado = Codificar(textoDecodificado);
-    document.getElementById("msg").value = textoCodificado;
+  items.forEach((element) => {
+    var textoProcurar = element[1];
+    var textoSubstituir = element[0];
+    texto = texto.replaceAll(textoProcurar, textoSubstituir);
   });
-  document.getElementById("btn-descripto").addEventListener("click", function () {
-      var textoCodificado = document.getElementById("input-texto").value;
-      var textoDecodificado = Decodificar(textoCodificado);
-      document.getElementById("msg").value = textoDecodificado;
-    });
-    document.getElementById("btn-copy").addEventListener("click", function () {
-      var inputMensagem = document.getElementById("msg");
-      copiar(inputMensagem);
-    });
+
+  return texto;
+}
+function copiar(campo) {
+  campo.select();
+  campo.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(campo.value);
+  alert("O texto foi copiado");
+}
+function entradaValida(texto) {
+  if(texto===""){
+    alert("Digite um texto para decodificar!");
+    return false;
+  }
+  if (texto.toLowerCase() != texto) {
+    alert("Deve usar apenas letras minusculas!");
+    return false;
+  }
+  const caracteresAcentos = "ÁÉÍÓÚáéíóúâêîôûàèìòùÇç";
+  var arrayCaracteresAcentos = caracteresAcentos.split("");
+
+  arrayCaracteresAcentos.forEach(function (caractere) {
+    if (texto.includes(caractere)) {
+      alert("Não pode utilizar acentos!");
+      return false;
+    }
+  });
+  return true;
+}
+document.getElementById("btn-cripto").addEventListener("click", function () {
+  var textoDecodificado = document.getElementById("input-texto").value;
+  if (!entradaValida(textoDecodificado)) {
+    return;
+  }
+  var textoCodificado = Codificar(textoDecodificado);
+  document.getElementById("msg").value = textoCodificado;
+});
+document.getElementById("btn-descripto").addEventListener("click", function () {
+  var textoCodificado = document.getElementById("input-texto").value;
+  var textoDecodificado = Decodificar(textoCodificado);
+  document.getElementById("msg").value = textoDecodificado;
+});
+document.getElementById("btn-copy").addEventListener("click", function () {
+  var inputMensagem = document.getElementById("msg");
+  copiar(inputMensagem);
+});
